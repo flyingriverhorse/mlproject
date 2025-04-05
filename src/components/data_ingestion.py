0c9551ea7eaab41e41966274aef992630e1cffb4
flyingriverhorse <buzzycarl@gmail.com>
2025-04-05 22:45:36 +0300

    train and tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,8 @@ from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 #                     ('cat_pipeline', cat_pipeline, categorical_columns) # Apply categorical pipeline to categorical columns
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
 
 @dataclass #decorator to automatically generate special methods like __init__ and __repr__
 class DataIngestionConfig:
@@ -64,5 +66,8 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion() #calling the function to initiate data ingestion
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
 #This will run the code when this file is run directly
